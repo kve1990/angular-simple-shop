53a2d1091a34ead33b0b2cac0a686739393612f8
Валерий Кондратко <kve@pkzdrav.ru>
2020-12-06 02:54:33 +0800

    style fixes

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ChangeDetectorRef, OnDestroy} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 import {BehaviorSubject} from 'rxjs';
 import {ProductService} from '../product.service';
 import {IProduct} from '../models/IProduct';
@@ -8,17 +9,32 @@ import {IProduct} from '../models/IProduct';
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit, OnDestroy {
 
+  public mobileQuery: MediaQueryList;
   public displayedColumns: string[] = ['name', 'price', 'count', 'totalProductCount'];
   public cartProducts$: BehaviorSubject<IProduct[]>;
   public total$: BehaviorSubject<number>;
 
-  constructor(private productService: ProductService) { }
+  private mobileQueryListener: () => void;
+
+  constructor(
+    private productService: ProductService,
+    private changeDetectorRef: ChangeDetectorRef,
+    public media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia('(min-width: 620px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
 
   ngOnInit(): void {
     this.cartProducts$ = this.productService.cartProducts$;
     this.total$ = this.productService.total$;
+  }
+
+  ngOnDestroy(): void{
+    this.mobileQuery.removeListener(this.mobileQueryListener);
   }
 
   addProductToCart(product: IProduct): void {
